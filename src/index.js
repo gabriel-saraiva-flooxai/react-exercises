2c7f435dedc65da16d6caf3bd94f2f1affe1fffe
gabriel-saraiva-flooxai <gabriel.saraiva.flooxai@gmail.com>
2025-05-12 16:49:13 -0300

    feat: child components #2

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import Pai from './components/Pai';
+import Filho from './components/Filho';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Pai nome='Jão' sobrenome='Bosco'>
-      {/* Como passa os componentes FILHOS aqui? */}
+      <Filho nome='Pedro' sobrenome='Bosco' />
+      <Filho nome='Jão' sobrenome='Bosco' />
+      <Filho nome='Carla' sobrenome='Bosco' />
     </Pai>
   </React.StrictMode>
 );
